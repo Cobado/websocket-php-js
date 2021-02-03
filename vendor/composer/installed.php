3622a457755f55c5f14d152ebb2b3aa6eca36fde
Nebado <ruslan.nbd@gmail.com>
2021-02-03 23:52:26 +0200

    Added Workerman with composer requre workerman/workerman.

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'b6afbb2c9ec1f9c6da0feb6c2f469807c4d3fb85',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b6afbb2c9ec1f9c6da0feb6c2f469807c4d3fb85',
+    ),
+    'workerman/workerman' => 
+    array (
+      'pretty_version' => 'v4.0.18',
+      'version' => '4.0.18.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '02930876526479c7e4123ef1dc8d23d509d40e72',
+    ),
+  ),
+);
