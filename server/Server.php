5aed0dc0f3fd99c242e1cccdb67d6f216571847c
Nebado <ruslan.nbd@gmail.com>
2021-02-04 00:05:42 +0200

    Implemented code for Server.

--- server/Server.php
+++ server/Server.php
@@ -1,3 +1,30 @@
 <?php
 
-//
+use Workerman\Worker;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+// Initializate entity of class Worker
+$wsWorker = new Worker('websocket://127.0.0.1:2346');
+
+// Quantity of processes for handling a connection from user
+$wsWorker->count = 4;
+
+// Callback for open a connection
+$wsWorker->onConnect = function ($connection) {
+    echo "New connection \n";
+};
+
+// Handle a message
+$wsWorker->onMessage = function ($connection, $data) use ($wsWorker) {
+    foreach($wsWorker->connections as $clientConnection) {
+        $clientConnection->send($data);
+    }
+};
+
+// Callback for close a connection
+$wsWorker->onClose = function ($connection) {
+    echo "Connection closed \n";
+};
+
+Worker::runAll();
